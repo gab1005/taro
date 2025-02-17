5e5ddcdcca6d542e737479f422e51580b9a46d2d
gab1005 <gabrielqwerty1@gmail.com>
2025-02-17 08:18:34 -0300

    deleted some files

--- src/lib/components/card/elements/Front.svelte
+++ src/lib/components/card/elements/Front.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	// import dataCardsTaro from '../../../utils/taro-arcana-cards.json';
 	import dataTarot from '$lib/utils/data-tarot.json';
 
 	interface IProps {
@@ -11,23 +10,19 @@
 		'basic-image': string;
 		'jujutsu-kaisen': string;
 	}
-
 	interface Time {
 		past: string;
 		present: string;
 		future: string;
 	}
-
 	interface Upright {
 		title: string;
 		time: Time;
 	}
-
 	interface Reversed {
 		title: string;
 		time: Time;
 	}
-
 	interface TarotCard {
 		id: number;
 		image: Image;
@@ -36,18 +31,14 @@
 	}
 
 	const data: TarotCard[] = dataTarot;
-
-
-
 	const indexArr = id - 1;
-	// const { title } = dataCardsTaro.major_arcana[indexArr];
 	const card = data[indexArr];
 </script>
 
-<div
-	class="flex w-full h-full"
->
-<img src={card.image['basic-image']} alt={card.upright.title} class="border-2 border-black rounded-2xl" >
-	<!-- <h2 class="font-bold text-tarot-black text-2xl italic">{card.reversed.title}</h2>
-	<p>{card.upright.time.present}</p> -->
+<div class="flex w-full h-full">
+	<img
+		src={card.image['basic-image']}
+		alt={card.upright.title}
+		class="border-2 border-black rounded-2xl"
+	/>
 </div>
--- src/lib/utils/random-1-22.js
+++
@@ -1,3 +0,0 @@
-export default function randomNumber() {
-  return Math.floor(Math.random() * 23) + 1;
-}
